7480de03f839167f36a62814e20fb2fb38f9f780
wruoruo <94418927+wruoruo@users.noreply.github.com>
2021-12-27 22:53:44 +0800

    Delete taobao/system directory

--- taobao/system/controller/VisitorController.java
+++
@@ -1,7 +0,0 @@
-package com.zsj.es.system.controller;
-
-import com.zsj.es.common.BaseController;
-
-public class VisitorController extends BaseController {
-
-}
--- taobao/system/dao/SysConfigMapper.java
+++
@@ -1,7 +0,0 @@
-package com.zsj.es.system.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.zsj.es.system.model.SysConfig;
-
-public interface SysConfigMapper extends BaseMapper<SysConfig> {
-}
